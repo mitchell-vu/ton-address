f8e483b7980912cf51530cbd489ca37323b4bf2e
Mitchell Vu <mitchell.vu0208@gmail.com>
2024-05-09 19:21:08 +0700

    Project Initialize

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Home: React.FC = () => {
+  return (
+    <section className='flex flex-col items-center justify-center gap-4 py-8 md:py-10'>
+      Hello
+    </section>
+  );
+};
+
+export default Home;
